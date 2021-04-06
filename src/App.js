6e56a1c7e009e15eccfb0056f0a332e875894a41
Mo Rob <31892724+morob1@users.noreply.github.com>
2021-04-06 17:50:44 +0100

    add components to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Header />
+      <div className="game-container">
+        <Figure  />
+        <WrongLetters />
+        <Word />
+      </div>
+    </>
   );
 }
 
